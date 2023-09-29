293959ffd20cbcc21c6e7eb04291c0e8e3665980
Alaa Eddine El Moudden <alaaeddine@Air-di-Alaa.wind3.hub>
2023-09-29 11:48:47 +0200

    app config and packages fixes

--- music_player/FirebaseConfig.js
+++ music_player/FirebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgEe4MUY975bKS1rzj3nkmmemxtNM8zAc",
@@ -16,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FRIEBASE_APP = initializeApp(firebaseConfig);
 export const FRIEBASE_AUTH = getAuth(FRIEBASE_APP);
+export const FRIEBASE_DB = getFirestore(FRIEBASE_APP);
